e0493515a55621d9a423ac29cd84a21652c5eee2
doa379 <doa379@gmail.com>
2020-08-30 10:40:15 +0100

    ;

--- factory.cpp
+++ factory.cpp
@@ -1,10 +1,49 @@
+class X
+{
+  public:
+  virtual void method(void) = 0;
+};
+
+class A : public X
+{
+  public:
+  void method(void) { };
+};
+
+class B : public X
+{
+  public:
+  void method(void) { };
+};
+
 class Factory
 {
   public:
-  Factory(...);
+  virtual X *create() = 0;
+  void method(void) { this->create(); };
+};
+
+class P : public Factory
+{
+  A a;
+
+  public:
+  X *create() { return &a; };
+};
+
+class Q : public Factory
+{
+  B b;
+
+  public:
+  X *create() { return &b; };
 };
 
 int main()
 {
+  P p;
+  p.create()->method();
+  Q q;
+  q.create()->method();
   return 0;
 }
